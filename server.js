e13a26036e617777684ba0b5054eba3b0e2fa7c0
Andrew Koster <andrewkoster95@gmail.com>
2021-07-17 22:36:58 -0400

    added function to delete notes and html calls to server.js

--- server.js
+++ server.js
@@ -18,13 +18,32 @@ app.get('/', (req,res) => {
 
 app.get('/notes', (req,res) => {
 	res.sendFile(path.join(__dirname, './public/notes.html'));
-});
+})
 
-app.post("/api/notes", (req, res) => {
-    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+app.post('/api/notes', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
     const newNotes = req.body;
     newNotes.id = uuid.v4();
     notes.push(newNotes);
-    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
     res.json(notes);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+    const notes = JSON.parse(fs.readFileSync('./db/db.json'));
+    const delNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
+    fs.writeFileSync('./db/db.json', JSON.stringify(delNote));
+    res.json(delNote);
+})
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+});
+
+app.get('/notes', function (req, res) {
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+app.listen(PORT, function () {
+    console.log('App listening on PORT: ' + PORT);
 });
