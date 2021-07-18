dcd3b5a60ec7acee130cc2cd38d61f3afa5c30df
Andrew Koster <andrewkoster95@gmail.com>
2021-07-17 22:02:19 -0400

    added middleware to server.js

--- server.js
+++ server.js
@@ -4,3 +4,10 @@ const path = require('path');
 const uuid = require('uuid');
 const fs = require('fs');
 const notes = require('./db/db.json');
+
+const app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
