569085d11f9d78ede079da94a03869b2b86785ca
Andrew Koster <andrewkoster95@gmail.com>
2021-07-17 22:23:36 -0400

    added functions that saves notes and also adds new notes

--- server.js
+++ server.js
@@ -10,4 +10,21 @@ var PORT = process.env.PORT || 3000;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("public"));
+app.use(express.static('public'));
+
+app.get('/', (req,res) => {
+	res.json(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req,res) => {
+	res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4();
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
+});
