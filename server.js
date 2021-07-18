50499ea4ca7ece6ec97990c57c5f7f11d9a099f2
Andrew Koster <andrewkoster95@gmail.com>
2021-07-17 21:58:13 -0400

    imported  nod modules to server.js

---
+++ server.js
@@ -0,0 +1,6 @@
+//Import modules
+const express = require('express');
+const path = require('path');
+const uuid = require('uuid');
+const fs = require('fs');
+const notes = require('./db/db.json');
